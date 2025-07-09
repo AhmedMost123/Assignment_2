31d511597a5165f8eb2f0494c7c330db13ea7ce2
AhmedMost123 <ahmedmost4444@gmail.com>
2025-07-10 00:59:21 +0300

    Create Multiplication-table.java

---
+++ Multiplication-table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       for(int i=0;i<=10;i++){
+           for(int j=0;j<=10;j++){
+               System.out.println(i+"x"+j+"="+(i*j));
+           }
+           System.out.println();
+       }
+    }
+}
