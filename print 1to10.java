abb8964947b4e1b6dc76c6ae92ceb70492c510ea
AhmedMost123 <ahmedmost4444@gmail.com>
2025-07-10 00:46:02 +0300

    Create print 1to10.java

---
+++ print 1to10.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+     //1 print numbers from 1-10   
+       for(int i=1;i<=10;i++){
+            System.out.println(i);
+        
+        }
+    }
+}
